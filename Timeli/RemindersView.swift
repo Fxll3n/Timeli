fc567ad290d3833f38a5bd9500bac3d18d7f30c1
Ben <benena2006@gmail.com>
2024-04-25 16:02:35 -0500

    Lost code, had to redo... Tried to implement a button to add the text into a textfield

--- Timeli/RemindersView.swift
+++ Timeli/RemindersView.swift
@@ -12,31 +12,46 @@ struct RemindersView: View {
     @State private var date = Date()
     @State private var title = ""
     @State private var content = ""
-
+//    @State private var task: String
+    
     var body: some View {
         VStack{
-                Text("Reminders View")
-                DatePicker("Choose ", selection: $date)
-                TextField("Enter Title", text: $title)
-                TextField("Enter Content", text: $content)
-                Button("Schedule Notification") {
-                    scheduleNotification(title: title, context: content, date: date)
-                }
+            Text("Reminders View")
+            DatePicker("Choose ", selection: $date)
+            TextField("Enter Title", text: $title)
+            TextField("Enter Content", text: $content)
+            Button("Schedule Notification") {
+                scheduleNotification(title: title, context: content, date: date)
+                
+                //                    TextField("Enter Example Reminder", text: $task)
+                //                        .multilineTextAlignment(.center
+                //                            .frame(width: 200, height: 30)
+                //                            .border(Color.black)
+                
+                // Button                    Button(action: {
+                //
+                //                    }, label: {
+                //                        Image(systemName: "plus")
+                //                    })
+            }
         }
     }
-
+    
     func scheduleNotification(title: String, context: String, date: Date) {
         let content = UNMutableNotificationContent()
         content.title = title
         content.subtitle = context
         content.sound = UNNotificationSound.default
-
+        
         let calendar = Calendar.current
         let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-
+        
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
+        
         UNUserNotificationCenter.current().add(request)
     }
+}
+#Preview{
+    RemindersView()
 }
