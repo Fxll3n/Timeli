565be0e71b215b7c4a5ac51d334116bade331651
Ben <benena2006@gmail.com>
2024-05-09 14:40:45 -0500

    created an Onboarding View

---
+++ Timeli/File.swift
@@ -0,0 +1,84 @@
+//
+//  File.swift
+//  Timeli
+//
+//  Created by Ben on 5/9/24.
+//
+
+//import SwiftUI
+//
+//struct Home: View{
+//    var body: some View {
+//        VStack {
+//            Text("Welcome to Timeli")
+//                .font(.title)
+//                .fontWeight(.heavy)
+//            Text("Follow our socials @timeli")
+//                .font(.body)
+//                .fontWeight(.medium)
+//        }
+//    }
+//}
+//struct OnboardingView: View {
+//    var body: some View {
+//        ZStack {
+//            VStack{
+//                HStack{
+//                    Text("Timeli ")
+//                        .font(.title)
+//                        .fontWeight(.semibold)
+//                        .kerning(1.4)
+//
+//                    Spacer()
+//
+//                    Button(action: {}, label: {
+//                        Text("Skip")
+//                            .fontWeight(.semibold)
+//                            .kerning(1.2)
+//                    })
+//                }.padding()
+//                    .foregroundColor(.black)
+//
+//                Spacer(minLength: 0)
+//
+//                Image("")
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .padding(.horizontal, 16)
+//                    .frame(height: 100)
+//
+//                Text("Get Started")
+//                    .font(.largeTitle)
+//                    .fontWeight(.bold)
+//                    .kerning(1.2)
+//                    .padding(.top)
+//                    .padding(.bottom, 5)
+//                    .foregroundColor(.blue)
+//
+//                Text("fwjflw jfnn lsefslejfs iljfnoesufnos efcesn vih uitun uihwthowej oweito weio tiowe ioweomwe dweo femfwf weko.")
+//                    .font(.body)
+//                    .fontWeight(.regular)
+//                    .kerning(1.2)
+//                    .padding([.leading, . trailing])
+//                    .multilineTextAlignment(.center)
+//
+//                Spacer(minLength: 0)
+//
+//                Button(action: {}, label: {
+//                    Text("Next")
+//                        .fontWeight(.semibold)
+//                        .frame(minWidth: 0, maxWidth: .infinity)
+//                        .padding()
+//                        .foregroundColor(.white)
+//                        .background(Color(.orange))
+//                        .cornerRadius(40)
+//                        .padding(.horizontal, 16)
+//                })
+//                }
+//            }
+//        }
+//    }
+//
+//#Preview {
+//    OnboardingView()
+//}
