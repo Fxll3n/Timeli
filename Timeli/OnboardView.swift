38069a41acfcb838310457e8648e23971e01e56e
Ben <benena2006@gmail.com>
2024-05-13 14:26:57 -0500

    edited onboard view

--- Timeli/OnboardView.swift
+++ Timeli/OnboardView.swift
@@ -21,63 +21,75 @@ struct Home: View{
 struct OnboardingView: View {
     var body: some View {
         ZStack {
-            VStack{
-                HStack{
-                    Text("Timeli ")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .kerning(1.4)
-                    
-                    Spacer()
-                    
-                    Button(action: {}, label: {
-                        Text("Skip")
-                            .fontWeight(.semibold)
-                            .kerning(1.2)
-                    })
-                }.padding()
-                    .foregroundColor(.black)
-                
-                Spacer(minLength: 0)
-                
-                Image("")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.horizontal, 16)
-                    .frame(height: 100)
-                
-                Text("Get Started")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .kerning(1.2)
-                    .padding(.top)
-                    .padding(.bottom, 5)
-                    .foregroundColor(.blue)
-                
-                Text("fwjflw jfnn lsefslejfs iljfnoesufnos efcesn vih uitun uihwthowej oweito weio tiowe ioweomwe dweo femfwf weko.")
-                    .font(.body)
-                    .fontWeight(.regular)
-                    .kerning(1.2)
-                    .padding([.leading, . trailing])
-                    .multilineTextAlignment(.center)
-                
-                Spacer(minLength: 0)
-                
-                Button(action: {}, label: {
-                    Text("Next")
-                        .fontWeight(.semibold)
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color(.orange))
-                        .cornerRadius(40)
-                        .padding(.horizontal, 16)
-                })
-                }
-            }
+            
         }
     }
+}
+
+struct ScreenView: View {
+    var image: String
+    var title: String
+    var details: String
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Timeli ")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .kerning(1.4)
+                
+                Spacer()
+                
+                Button(action: {}, label: {
+                    Text("Skip")
+                        .fontWeight(.semibold)
+                        .kerning(1.2)
+                })
+            }.padding()
+                .foregroundColor(.black)
+            
+            Spacer(minLength: 0)
+            
+            Image("")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.horizontal, 16)
+                .frame(height: 100)
+            
+            Spacer(minLength: 80)
+            
+            Text("Get Started")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .kerning(1.2)
+                .padding(.top)
+                .padding(.bottom, 5)
+                .foregroundColor(.blue)
+            
+            Text("fwjflw jfnn lsefslejfs iljfnoesufnos efcesn vih uitun uihwthowej oweito weio tiowe ioweomwe dweo femfwf weko.")
+                .font(.body)
+                .fontWeight(.regular)
+                .kerning(1.2)
+                .padding([.leading, . trailing])
+                .multilineTextAlignment(.center)
+            
+            Spacer(minLength: 0)
+            
+            Button(action: {}, label: {
+                Text("Next")
+                    .fontWeight(.semibold)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color(.orange))
+                    .cornerRadius(40)
+                    .padding(.horizontal, 16)
+            })
+        }
+    }
+}
 
 #Preview {
-    OnboardingView()
+    Home()
 }
