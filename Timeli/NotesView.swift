58e1541228a212d032b66fb2a5097a902af03ced
Matthew Dudzinski <42253757+Resident-Fortress@users.noreply.github.com>
2024-05-16 16:54:30 -0500

    Added more comments

--- Timeli/NotesView.swift
+++ Timeli/NotesView.swift
@@ -44,22 +44,22 @@ struct NotesView: View {
     }
     
     func loadMD() {
-        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let fileURL = documentsDirectory.appendingPathComponent("note.md")
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first! // Tells the app that the path for the documents would be inside of the fileManager intializer.
+        let fileURL = documentsDirectory.appendingPathComponent("note.md") // Looks for the directory a document is stored in and looks for the Path. Example /var/mobile/Containers/Data/Application/your-app-UUID)
         
         do {
-            mdContent = try String(contentsOf: fileURL, encoding: .utf8)
+            mdContent = try String(contentsOf: fileURL, encoding: .utf8) // Reads the content inside of fileURL
             print("Successfully loaded file!")
         } catch {
             print("Error loading the note \(error)")
         }
     }
     func saveMD() {
-        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let fileURL = documentsDirectory.appendingPathComponent("note.md")
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first! // Tells the app that the path for the documents would be inside of the fileManager intializer.
+        let fileURL = documentsDirectory.appendingPathComponent("note.md") // Looks for the directory a document is stored in and looks for the Path. Example /var/mobile/Containers/Data/Application/your-app-UUID)
         
         do {
-            try mdContent.write(to:fileURL, atomically: true, encoding: .utf8)
+            try mdContent.write(to:fileURL, atomically: true, encoding: .utf8) // Writes the content inside of mdContent to the variable fileURL
             print("Successfully saved file!")
         } catch {
             print("Error saving the file \(error)")
